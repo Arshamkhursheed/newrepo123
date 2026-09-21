5016ed3276f17ca8207d5c0db81fe1d0ff5bc920
arsham khursheed <arshamkhursheed@gmail.com>
2026-09-21 09:14:24 +0530

    adding somthing

---
+++ react_app/src/bbd.js
@@ -0,0 +1,8 @@
+function bbd() {
+    return (
+        <h1>Welcome to My Website</h1>
+    )
+};
+
+
+export default bbd;
--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-import bodyy from './App';
+import bbd from './bbd.js';
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <bbd />
   </React.StrictMode>
 );
+
 
 
